97130f6f5b39d75175feb8794ca6f93f7242ba35
Neil Supillo <128984447+NeilSupillo@users.noreply.github.com>
2023-11-22 15:56:30 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://keeper-alpha.vercel.app/"],
+    origin: ["https://keeper-frontend-sigma.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
